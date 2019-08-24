e155216e5164863f4585e5556c7302c4cf36256f
Aron Jon Prenovost <Aroniascoder@gmail.com>
2019-08-24 11:19:23 -0700

    saving username

--- index.js
+++ index.js
@@ -9,51 +9,22 @@ app.get('/', function (req, res) {
 var users = [];
 
 io.on('connection', function (socket) {
+    socket.on('newuser', function (usernameInput) {
 
-    // { address: '::1', family: 'IPv6', port: 61604 }
-    const socket_data = socket.request.connection._peername;
-    socket_data.alias = undefined;
+        // { address: '::1', family: 'IPv6', port: 61604 }
+        const socket_data = socket.request.connection._peername;
+        socket_data.alias = usernameInput;
 
-    // handle duplicate connections 
-    // for (var i = 0; i < users.length; i++) {
-    //     if (users[i].port === socket_data.port) {
-    //         io.emit('duplicateUser');
-    //     }
-    // }
+        // handle duplicate connections 
+        // for (var i = 0; i < users.length; i++) {
+        //     if (users[i].port === socket_data.port) {
+        //         io.emit('duplicateUser');
+        //     }
+        // }
 
-    users.push(socket_data);
+        users.push(socket_data);
 
-    // get names of users 
-    var userNames = [];
-    users.forEach((e) => {
-        userNames.push(e.port);
-    });
-    io.sockets.emit('userCount', {
-        userCount: userNames.length,
-        userList: userNames
-    });
-
-    // chat 
-    socket.on('chat message', function (msg) {
-        io.emit('chatMessage', socket_data.port + ': ' + msg);
-        socket_data.message = msg;
-    });
-
-    // notify when user enters room 
-    socket.join('room', function () {
-        io.emit('userJoined', socket_data.port + ' joined the room');
-    });
-
-    // notify when user exits room 
-    socket.on('disconnect', function () {
-
-        io.emit('userLeft', socket_data.port + ' left the room');
-        for (var i = 0; i < users.length; i++) {
-            if (users[i].port === socket_data.port) {
-                users.splice([i], 1);
-            }
-        }
-
+        // get names of users 
         var userNames = [];
         users.forEach((e) => {
             userNames.push(e.port);
@@ -62,13 +33,43 @@ io.on('connection', function (socket) {
             userCount: userNames.length,
             userList: userNames
         });
-    });
 
-    // show when user is typing 
-    socket.on('typing', function (data) {
-        socket.broadcast.emit('typing', socket_data.port + ' is ' + data);
-    });
+        // chat 
+        socket.on('chat message', function (msg) {
+            io.emit('chatMessage', socket_data.port + ': ' + msg);
+            socket_data.message = msg;
+        });
 
+        // notify when user enters room 
+        socket.join('room', function () {
+            io.emit('userJoined', socket_data.port + ' joined the room');
+        });
+
+        // notify when user exits room 
+        socket.on('disconnect', function () {
+
+            io.emit('userLeft', socket_data.port + ' left the room');
+            for (var i = 0; i < users.length; i++) {
+                if (users[i].port === socket_data.port) {
+                    users.splice([i], 1);
+                }
+            }
+
+            var userNames = [];
+            users.forEach((e) => {
+                userNames.push(e.port);
+            });
+            io.sockets.emit('userCount', {
+                userCount: userNames.length,
+                userList: userNames
+            });
+        });
+
+        // show when user is typing 
+        socket.on('typing', function (data) {
+            socket.broadcast.emit('typing', socket_data.port + ' is ' + data);
+        });
+    });
 });
 
 http.listen(3000, function () {
