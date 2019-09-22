42732439d761b82deff879d5f2f4fe4692d0d183
Aron Jon Prenovost <Aroniascoder@gmail.com>
2019-09-22 16:38:52 -0700

    added drawing capabilities

--- public/js/client.js
+++ public/js/client.js
@@ -191,68 +191,68 @@ socket.on('connect', function () {
     // });
 
     // add timestamp somewhere in chatlog 
-    // appendLi(formatDate(new Date()), messages, 'generic');
+    appendLi(formatDate(new Date()), messages, 'generic');
 
-    // draw 
-    // var mouse = {
-    //     click: false,
-    //     move: false,
-    //     pos: { x: 0, y: 0 },
-    //     pos_prev: false
-    // };
-    // var btn = document.getElementById('btn');
-    // // get canvas element and create context
-    // var canvas = document.getElementById('drawing');
-    // var context = canvas.getContext('2d');
-    // var width = window.innerWidth;
-    // var height = window.innerHeight;
-    // canvas.width = width;
-    // canvas.height = height;
+    // draw
+    var mouse = {
+        click: false,
+        move: false,
+        pos: { x: 0, y: 0 },
+        pos_prev: false
+    };
+    var btn = document.getElementById('btn');
+    // get canvas element and create context
+    var canvas = document.getElementById('drawing');
+    var context = canvas.getContext('2d');
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    canvas.width = width;
+    canvas.height = height;
 
-    // // register mouse event handlers
-    // canvas.onmousedown = function (e) { mouse.click = true; };
-    // canvas.onmouseup = function (e) { mouse.click = false; };
+    // register mouse event handlers
+    canvas.onmousedown = function (e) { mouse.click = true; };
+    canvas.onmouseup = function (e) { mouse.click = false; };
 
-    // canvas.onmousemove = function (e) {
-    //     // normalize mouse position to range 0.0 - 1.0
-    //     mouse.pos.x = e.clientX / width;
-    //     mouse.pos.y = e.clientY / (height - 45);
-    //     mouse.move = true;
-    // };
+    canvas.onmousemove = function (e) {
+        // normalize mouse position to range 0.0 - 1.0
+        mouse.pos.x = e.clientX / width;
+        mouse.pos.y = e.clientY / (height - 45);
+        mouse.move = true;
+    };
 
 
-    // btn.addEventListener('click', function (e) {
-    //     // add event listerner 
-    //     socket.emit('clearCanvas');
-    // });
+    btn.addEventListener('click', function (e) {
+        // add event listerner 
+        socket.emit('clearCanvas');
+    });
 
-    // socket.on('clearCanvas', function (data) {
-    //     context.clearRect(0, 0, canvas.width, canvas.height);
-    // });
+    socket.on('clearCanvas', function (data) {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+    });
 
-    // // socket data below 
-    // // draw line received from server
-    // socket.on('drawLine', function (data) {
-    //     var line = data.line;
-    //     context.beginPath();
-    //     context.moveTo(line[0].x * width, line[0].y * height);
-    //     context.lineTo(line[1].x * width, line[1].y * height);
-    //     context.stroke();
-    // });
+    // socket data below 
+    // draw line received from server
+    socket.on('drawLine', function (data) {
+        var line = data.line;
+        context.beginPath();
+        context.moveTo(line[0].x * width, line[0].y * height);
+        context.lineTo(line[1].x * width, line[1].y * height);
+        context.stroke();
+    });
 
-    // // main loop, running every 25ms
-    // function mainLoop() {
+    // main loop, running every 25ms
+    function mainLoop() {
 
-    //     // check if the user is drawing
-    //     if (mouse.click && mouse.move && mouse.pos_prev) {
-    //         // send line to to the server
-    //         socket.emit('drawLine', { line: [mouse.pos, mouse.pos_prev] });
-    //         mouse.move = false;
-    //     }
-    //     mouse.pos_prev = { x: mouse.pos.x, y: mouse.pos.y };
-    //     setTimeout(mainLoop, 25);
-    // }
-    // mainLoop();
+        // check if the user is drawing
+        if (mouse.click && mouse.move && mouse.pos_prev) {
+            // send line to to the server
+            socket.emit('drawLine', { line: [mouse.pos, mouse.pos_prev] });
+            mouse.move = false;
+        }
+        mouse.pos_prev = { x: mouse.pos.x, y: mouse.pos.y };
+        setTimeout(mainLoop, 25);
+    }
+    mainLoop();
 
     // end draw 
 
