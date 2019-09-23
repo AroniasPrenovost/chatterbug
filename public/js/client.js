cd9f11674c3b1c1d4a77cc8a8e89c02cd17aa0d4
Aron Jon Prenovost <Aroniascoder@gmail.com>
2019-09-22 21:02:32 -0700

    updates

--- public/js/client.js
+++ public/js/client.js
@@ -216,7 +216,7 @@ socket.on('connect', function () {
     canvas.onmousemove = function (e) {
         // normalize mouse position to range 0.0 - 1.0
         mouse.pos.x = e.clientX / width;
-        mouse.pos.y = e.clientY / (height - 45);
+        mouse.pos.y = e.clientY / (height - 0);
         mouse.move = true;
     };
 
