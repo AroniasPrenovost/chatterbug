f02e97534fe61c49a5145af1c7c09f6625dbcf77
Aron Jon Prenovost <Aroniascoder@gmail.com>
2019-11-04 19:37:00 -0800

    fixing issue with css and express

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
